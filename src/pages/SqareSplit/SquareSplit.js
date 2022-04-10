c97dd9b54c001f4037ad213d1cd7ee8e4102eb8b
vinuppriya balachandran <vinuppriya.b@gmail.com>
2022-04-10 23:21:27 +0530

    changes

--- src/pages/SqareSplit/SquareSplit.js
+++ src/pages/SqareSplit/SquareSplit.js
@@ -1,80 +1,79 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 import "./SquareSplit.css";
 
-
-
-
-
 const SquareSplit = () => {
+  const [isClick, setIsClick] = useState(false);
 
-const [isClick,setIsClick]=useState(false);
-
-  const handleClick=()=>{
-    setIsClick(true)
-  }
+  const handleClick = () => {
+    setIsClick(true);
+  };
 
   return (
-    <div className='SquareSplit_game'>
+    <div className="SquareSplit_game">
       <h1>BOX SPLITING</h1>
-      <div className='square_board' onClick={handleClick}>{isClick?<CreateSquare/>:""}
+      <div className="square_board" onClick={handleClick}>
+        {isClick ? <CreateSquare /> : ""}
       </div>
     </div>
-  )
-}
+  );
+};
 
-const CreateSquare=()=>{
- const [square,setSqaure]=useState([true,true,true,true])
-  return(
-<div className='Create_Square'>{square.map((val,index)=><Square val={val} index={index} key={index}/>)}</div>
-  )
-}
+const CreateSquare = () => {
+  const [square, setSqaure] = useState([true, true, true, true]);
+  return (
+    <div className="Create_Square">
+      {square.map((val, index) => (
+        <Square val={val} index={index} key={index} />
+      ))}
+    </div>
+  );
+};
 
-const Square=({index})=>{
+const Square = ({ index }) => {
   const style = {
     0: {
-      borderRight:"1px solid black",
-      height:'calc(50% - 0.5px)',
-      width: 'calc(50% - 0.5px)',
-     
-      margin:"0px",
+      borderRight: "1px solid black",
+      height: "calc(50% - 0.5px)",
+      width: "calc(50% - 0.5px)",
+
+      margin: "0px",
       padding: "0px",
     },
     1: {
-      height:'calc(50% - 0.5px)',
-      width: 'calc(50% - 0.5px)',
+      height: "calc(50% - 0.5px)",
+      width: "calc(50% - 0.5px)",
       padding: "0px",
     },
     2: {
-      height:'calc(50% - 0.5px)', 
-      width: 'calc(50% - 0.5px)',
-      borderTop:"1px solid black",
-      borderRight:"1px solid black",
-      margin:"0px",
+      height: "calc(50% - 0.5px)",
+      width: "calc(50% - 0.5px)",
+      borderTop: "1px solid black",
+      borderRight: "1px solid black",
+      margin: "0px",
       padding: "0px",
     },
     3: {
-      height:'calc(50% - 0.5px)',
-      width: 'calc(50% - 0.5px)',
-      borderTop:"1px solid black",
-      margin:"0px",
+      height: "calc(50% - 0.5px)",
+      width: "calc(50% - 0.5px)",
+      borderTop: "1px solid black",
+      margin: "0px",
       padding: "0px",
-    }
-  }
-  console.log(index)
-  const [isClick,setIsClick]=useState(false);
+    },
+  };
+  console.log(index);
+  const [isClick, setIsClick] = useState(false);
 
-  const handleClick=()=>{
-    setIsClick(true)
-  }
+  const handleClick = () => {
+    setIsClick(true);
+  };
 
-  return(
-    <div style={style[index]}  onClick={handleClick}>
-      {isClick?<CreateSquare />:""}
-
+  return (
+    <div style={style[index]} onClick={handleClick}>
+      {isClick ? <CreateSquare /> : ""}
     </div>
-  )
-}
+  );
+};
 
-export default SquareSplit
+export default SquareSplit;
 
 // className={index%2===0 ? "right_line":"small_square" && index===0?"bottom_line" :"small_square" }
