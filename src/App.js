5b34c94c6917b7a7b1e031cc73ce5da2d460024c
vinuppriya balachandran <vinuppriya.b@gmail.com>
2022-04-10 21:43:49 +0530

    home

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+
+import { Switch, Route,  Redirect } from "react-router-dom";
 import Tictactoe from './pages/tictactoe/Tictactoe';
 import SquareSplit from './pages/SqareSplit/SquareSplit';
 import TreeReply from './pages/TreeReply/TreeReply';
-
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+ import Toolbar from "@mui/material/Toolbar";
+// import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { useHistory } from "react-router-dom";
+// import IconButton from '@mui/material/IconButton';
+// import MenuIcon from '@mui/icons-material/Menu';
+// import { Mode } from "@mui/icons-material";
+import HomeIcon from '@mui/icons-material/Home';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import TheatersIcon from '@mui/icons-material/Theaters';
+import Header from './pages/Header/Header';
+import Home from './pages/Home/Home';
 function App() {
+  const history = useHistory();
   return (
     <div className="App">
-      <TreeReply/>
-      <SquareSplit/>
-     <Tictactoe/>
+      
+      <Header/>
+
+       <Switch>
+        <Route path="/treereply">
+        <TreeReply/>
+        </Route>
+
+        <Route path="/boxsplitting">
+        <SquareSplit/>
+        </Route>
+        <Route exact path="/tictactoe">
+        <Tictactoe/>
+        </Route>
+       
+        <Route path="/">
+        <Home/>
+        </Route>
+        </Switch>
+      
+     
+    
     </div>
   );
 }
---
+++ src/pages/Header/Header.js
@@ -0,0 +1,181 @@
+import React, { useState } from "react";
+import "./Header.css";
+import Button from "@mui/material/Button";
+import { useHistory } from "react-router";
+import Popover from "@mui/material/Popover";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+
+// Recruiter Nav bar
+
+const Header = () => {
+const [Select,setSelect]=useState([true,false,false,false])
+  const history = useHistory();
+
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
+
+  //Handle Logout Function to set free the user details store in local Storage
+
+  
+  return (
+    <div className="Header_container">
+      <div className="header_left">
+        <h3>ADMAVIN TASK</h3>
+      </div>
+      <div className="header_right bigscreen">
+        <Button
+          className={Select[0] ? " selected " : "title "}
+          variant="text"
+          onClick={() => {
+            setSelect([true, false, false, false]);
+            history.push("/");
+          }}
+        >
+          home
+        </Button>
+        <Button
+          variant="text"
+          onClick={() => {
+            setSelect([false, true, false, false]);
+            history.push("/treereply");
+          }}
+          className={Select[1] ? " selected " : "title "}
+        >
+         Tree Reply
+        </Button>
+        <Button
+          variant="text"
+          onClick={() => {
+            setSelect([false, false, true, false]);
+            history.push("/boxsplitting");
+          }}
+          className={Select[2] ? " selected " : "title "}
+        >
+          Box Splitting
+        </Button>
+
+        <Button
+          variant="text"
+          onClick={() => {
+            setSelect([false, false, false, true]);
+            history.push("/tictactoe");
+          }}
+          className={Select[3] ? " selected " : "title "}
+        >
+          Tic Tac Toe
+        </Button>
+      </div>
+      <div className="smallscreen">
+        <MoreVertIcon
+          className="More_icon"
+          variant="contained"
+          onClick={handleClick}
+        />
+
+        <Popover
+          className="popover"
+          id={id}
+          open={open}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "left",
+          }}
+        >
+          <List
+            className="popover_list"
+            sx={{
+              width: "500px",
+              color: "rgba(234, 180, 30)",
+              bgcolor: "rgba(8, 46, 78)",
+              fontSize: "10px",
+
+              // fontWeight: "bold",
+            }}
+            component="nav"
+            aria-labelledby="nested-list-subheader"
+          >
+            <ListItemButton
+              onClick={() => {
+                handleClose();
+              }}
+            >
+              <div
+                className={Select[0] ? " selected " : "title "}
+                onClick={() => {
+                  setSelect([true, false, false, false]);
+                  history.push("/treereply");
+                }}
+              >
+                Home
+              </div>
+            </ListItemButton>
+            <ListItemButton
+              onClick={() => {
+                handleClose();
+              }}
+            >
+              <div
+                className={Select[1] ? " selected" : "title"}
+                onClick={() => {
+                  setSelect([false, true, false, false]);
+
+                  history.push("/treereply");
+                }}
+              >
+               Tree Reply
+              </div>
+            </ListItemButton>
+
+            <ListItemButton
+              onClick={() => {
+                handleClose();
+              }}
+            >
+              <div
+                className={Select[2] ? " selected" : "title"}
+                onClick={() => {
+                  setSelect([false, false, true, false]);
+                  history.push("/boxsplitting");
+                }}
+              >
+                Box Splitting
+              </div>
+            </ListItemButton>
+
+            <ListItemButton
+              onClick={() => {
+                handleClose();
+              }}
+            >
+              <div
+                className={Select[3] ? " selected" : "title"}
+                onClick={() => {
+                    setSelect([false, false, false, true]);
+                    history.push("/tictactoe");
+                  }}
+              >
+                Tic Tac Toe
+              </div>
+            </ListItemButton>
+          </List>
+        </Popover>
+      </div>
+    </div>
+  );
+};
+
+export default Header
